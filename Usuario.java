28c8cc72a36db402d9ee25cd65aa5a86a312c4c4
JitteryWinner <davidaberganza@gmail.com>
2025-09-14 20:52:23 -0600

    Update Usuario.java
    Se conecto la clase Controlador de Usuario con la clase Usuario.

--- Usuario.java
+++ Usuario.java
@@ -1,33 +1,51 @@
 import java.util.ArrayList;
+import java.util.List;
+
 public class Usuario{
     private String nombre; 
     private String correo;
     private String contrasena;
     private ArrayList<Notificacion> notificaciones = new ArrayList<Notificacion>();
+
     public Usuario(String nombre, String correo, String contrasena){
         this.nombre = nombre;
         this.correo = correo;
         this.contrasena = contrasena;
     }
+
     public String getNombre(){
         return this.nombre;
     }
+
     public void setNombre(String nombre){
         this.nombre = nombre;
     }
+
     public Boolean iniciarSesion(String correo, String contrasena){
-        return (this.correo.equals(correo) && this.contrasena.equals(contrasena)) ? true : false;
+        return (this.correo.equals(correo) && this.contrasena.equals(contrasena));
     }
+
     public Boolean cambiarContrasena(String nuevaContrasena){
-        return !(this.contrasena.equals(nuevaContrasena));
+        if (!this.contrasena.equals(nuevaContrasena)) {
+            this.contrasena = nuevaContrasena;
+            return true;
+        }
+        return false;
     }
+
     public void enviarSugerencia(String sugerencia){
-        //se completara de acuerdo como se maneje el controlador
+
     }
+
     public void addNotificacion(Notificacion notificacion){
         notificaciones.add(notificacion);
     }
+
     public void delNotificacion(Notificacion notificacion){
-        notificaciones.remove(notificaciones.indexof(notificacion));
+        notificaciones.remove(notificacion);
+    }
+
+    public List<Notificacion> getNotificaciones() {
+        return new ArrayList<>(notificaciones);
     }
 }
